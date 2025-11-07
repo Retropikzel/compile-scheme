41b4276caffd75c2eff84df160ca500df36a7cd8
retropikzel <retropikzel@iki.fi>
2025-11-07 20:27:36 +0200

    Take the target option out of manpage and bash completion as it is not ready yet

--- bash_completion.sh
+++ bash_completion.sh
@@ -6,26 +6,26 @@ _compile_scheme()
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     shortopts="-A -I"
     longopts="--list-r6rs --list-r7rs --list-all --help --version"
-    targets="unix windows php"
+    targets="unix" # windows php"
     outputfound=false
     for i in "${COMP_WORDS[@]}"; do
         if [[ "$i" == "-o" ]] ; then
             outputfound=true
         fi
     done
-    targetfound=false
-    for i in "${COMP_WORDS[@]}"; do
-        if [[ "$i" == "-t" ]] ; then
-            targetfound=true
-        fi
+    #targetfound=false
+    #for i in "${COMP_WORDS[@]}"; do
+        #if [[ "$i" == "-t" ]] ; then
+            #targetfound=true
+        #fi
     done
 
     if [[ "$outputfound" == "false" ]]; then
         shortopts="${shortopts} -o"
     fi
-    if [[ "$targetfound" == "false" ]]; then
-        shortopts="${shortopts} -t"
-    fi
+    #if [[ "$targetfound" == "false" ]]; then
+        #shortopts="${shortopts} -t"
+    #fi
 
     opts="${shortopts} ${longopts}"
 
@@ -38,15 +38,15 @@ _compile_scheme()
     elif [[ ${prev} == -A ]] ; then
         COMPREPLY=()
         return 0
-    elif [[ ${prev} == -t ]] ; then
-        COMPREPLY=( $(compgen -W "${targets}" -- ${cur}) )
-        return 0
+    #elif [[ ${prev} == -t ]] ; then
+        #COMPREPLY=( $(compgen -W "${targets}" -- ${cur}) )
+        #return 0
     elif [[ ${cur} == -o ]] ; then
         COMPREPLY=("")
         return 0
-    elif [[ ${cur} == -t ]] ; then
-        COMPREPLY=("")
-        return 0
+    #elif [[ ${cur} == -t ]] ; then
+        #COMPREPLY=("")
+        #return 0
     elif [[ ${prev} == *.sps ]] ; then
         COMPREPLY=( $(compgen -W "${shortopts}" -- ${cur}) )
         return 0

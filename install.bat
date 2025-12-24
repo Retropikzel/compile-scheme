9d0aa5dd83fd3cb9234b8c31a512db7dda02a275
retropikzel <retropikzel@iki.fi>
2025-12-24 11:45:07 +0200

    Working on windows version

--- install.bat
+++ install.bat
@@ -4,11 +4,11 @@ echo Installing...
 
 if not exist "%ProgramFiles%\compile-scheme" mkdir "%ProgramFiles%\compile-scheme"
 if not exist "%ProgramFiles%\compile-scheme\libs" mkdir "%ProgramFiles%\compile-scheme\libs"
-copy compile-scheme.bat "%ProgramFiles%\compile-scheme\"
-copy compile-scheme.scm "%ProgramFiles%\compile-scheme\"
-copy libs\implementations.sld "%ProgramFiles%\compile-scheme\libs\"
-copy libs\library-util.sld "%ProgramFiles%\compile-scheme\libs\"
-copy libs\util.sld "%ProgramFiles%\compile-scheme\libs\"
+copy compile-scheme.bat "%ProgramFiles%\compile-scheme\" > nul
+copy compile-scheme.scm "%ProgramFiles%\compile-scheme\" > nul
+copy libs\implementations.sld "%ProgramFiles%\compile-scheme\libs\" > nul
+copy libs\library-util.sld "%ProgramFiles%\compile-scheme\libs\" > nul
+copy libs\util.sld "%ProgramFiles%\compile-scheme\libs\" > nul
 ECHO.%PATH% | FIND /I "compile-scheme" > nul || setx PATH "%PATH%;%ProgramFiles%\compile-scheme" /m
 
 echo Install complete

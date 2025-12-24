8e5aa3414a632608e2ab065836fa324032244979
retropikzel <retropikzel@iki.fi>
2025-12-24 11:43:38 +0200

    Working on windows version

--- install.bat
+++ install.bat
@@ -9,6 +9,6 @@ copy compile-scheme.scm "%ProgramFiles%\compile-scheme\"
 copy libs\implementations.sld "%ProgramFiles%\compile-scheme\libs\"
 copy libs\library-util.sld "%ProgramFiles%\compile-scheme\libs\"
 copy libs\util.sld "%ProgramFiles%\compile-scheme\libs\"
-ECHO.%PATH% | FIND /I "compile-scheme" > nul || setx PATH "%PATH%;%ProgramFiles%\compile-scheme"
+ECHO.%PATH% | FIND /I "compile-scheme" > nul || setx PATH "%PATH%;%ProgramFiles%\compile-scheme" /m
 
 echo Install complete
